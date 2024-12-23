4cb4f1e04ff696ba9b1c033e8d930378c5aa0a10
AlexSim199513 <alex199513@gmail.com>
2024-12-22 20:52:53 -0500

    Added HealthzHandler func

--- main.go
+++ main.go
@@ -5,19 +5,25 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("."))
-
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
